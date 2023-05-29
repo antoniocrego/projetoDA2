187761d5bcdce76808b37c9cdc49108c2584dca1
DANIEL <danielcb7@hotmail.com>
2023-05-29 23:30:10 +0100

    Integration of the backtracking on the main menu

--- src/Program.cpp
+++ src/Program.cpp
@@ -1,5 +1,7 @@
 #include "Program.h"
 
+using namespace std;
+
 Program::Program() {
     this->currentMenuPage=0;
     createMenu();
@@ -54,12 +56,39 @@ void Program::draw(){
 void Program:: run() {
     while (this->currentMenuPage != -1) {
         draw(); //draw the current menu
-        string path = "";
+        string file_path = "";
+        clock_t start;
+        clock_t stop;
+        vector<int> path = {0};
+        double max_double = numeric_limits<double>::max();
+        double *min_cost = &max_double;
         int option = menus.at(currentMenuPage).getOption();
         switch (this->currentMenuPage) {
             case 0: // Is on main menu
                 switch (option) {
                     case 1:
+                        for (auto vertex: network.getCurrentGraph().getVertexSet()){
+                            if (vertex->getId()==0) vertex->setVisited(true);
+                            vertex->setVisited(false);
+                        }
+                        start = clock();
+                        network.backtracking(network.getCurrentGraph(),*min_cost,0,0,path, {0});
+                        stop = clock();
+
+                        clear();
+
+                        cout << "The min cost calculated by backtracking was: " << *min_cost << endl;
+                        cout << "The path found was: " << endl;
+                        for(int i = 0; i < path.size(); i++){
+                            if(i == 0) cout << path[i];
+                            else{
+                                cout << "->" << path[i];
+                            }
+                        }
+                        cout << endl;
+                        cout << "Running time of backtracking: " << (float)(stop-start)/CLOCKS_PER_SEC << " seconds" << endl;
+
+                        wait();
                         break;
                     case 2:
                         break;
@@ -87,83 +116,83 @@ void Program:: run() {
                 }
                 break;
             case 2:
-                path = "Toy-Graphs/";
+                file_path = "Toy-Graphs/";
                 switch (option) {
                     case 1:
-                        path += "shipping.csv";
+                        file_path += "shipping.csv";
                         break;
                     case 2:
-                        path += "stadiums.csv";
+                        file_path += "stadiums.csv";
                         break;
                     case 3:
-                        path += "tourism.csv";
+                        file_path += "tourism.csv";
                         break;
                 }
-                if(path != "Toy-Graphs/"){
-                    network.readDataset(path, false);
+                if(file_path != "Toy-Graphs/"){
+                    network.readDataset(file_path, false);
                     currentMenuPage = 0;
                 }
                 break;
             case 3:
-                path = "Extra_Fully_Connected_Graphs/edges_";
+                file_path = "Extra_Fully_Connected_Graphs/edges_";
                 switch (option) {
                     case 1:
-                        path += "25.csv";
+                        file_path += "25.csv";
                         break;
                     case 2:
-                        path += "50.csv";
+                        file_path += "50.csv";
                         break;
                     case 3:
-                        path += "75.csv";
+                        file_path += "75.csv";
                         break;
                     case 4:
-                        path += "100.csv";
+                        file_path += "100.csv";
                         break;
                     case 5:
-                        path += "200.csv";
+                        file_path += "200.csv";
                         break;
                     case 6:
-                        path += "300.csv";
+                        file_path += "300.csv";
                         break;
                     case 7:
-                        path += "400.csv";
+                        file_path += "400.csv";
                         break;
                     case 8:
-                        path += "500.csv";
+                        file_path += "500.csv";
                         break;
                     case 9:
-                        path += "600.csv";
+                        file_path += "600.csv";
                         break;
                     case 10:
-                        path += "700.csv";
+                        file_path += "700.csv";
                         break;
                     case 11:
-                        path += "800.csv";
+                        file_path += "800.csv";
                         break;
                     case 12:
-                        path += "900.csv";
+                        file_path += "900.csv";
                         break;
                 }
-                if(path != "Extra_Fully_Connected/edges_") {
-                    network.readDataset(path, false);
+                if(file_path != "Extra_Fully_Connected/edges_") {
+                    network.readDataset(file_path, false);
                     currentMenuPage = 0;
                 }
                 break;
             case 4:
-                path = "Real-world Graphs/";
+                file_path = "Real-world Graphs/";
                 switch (option) {
                     case 1:
-                        path += "graph1/";
+                        file_path += "graph1/";
                         break;
                     case 2:
-                        path += "graph2/";
+                        file_path += "graph2/";
                         break;
                     case 3:
-                        path += "graph3/";
+                        file_path += "graph3/";
                         break;
                 }
-                if(path != "Real-world Graphs/") {
-                    network.readDataset(path, true);
+                if(file_path != "Real-world Graphs/") {
+                    network.readDataset(file_path, true);
                     currentMenuPage = 0;
                 }
                 break;
--- src/main.cpp
+++ src/main.cpp
@@ -2,25 +2,9 @@
 #include "Program.h"
 
 int main(){
-    Network network = Network();
-    network.readDataset("Toy-Graphs/shipping.csv", false);
-    std::vector<int> path = {0};
-    std::vector<double> dist;
-    Graph c = network.getCurrentGraph();
 
-    for (auto vertex: c.getVertexSet()){
-        if (vertex->getId()==0) vertex->setVisited(true);
-        vertex->setVisited(false);
-    }
-    double max_cost = std::numeric_limits<double>::max();
-    double* min_cost = &max_cost;
-
-    network.backtracking(c,*min_cost,0,0,path,{0});
-
-    cout << *min_cost << endl;
-/*
     Program p = Program();
     p.run();
-*/
+
     return 0;
 }
