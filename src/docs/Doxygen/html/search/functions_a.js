af1d990263c22d34739fd4a91c114711750513ad
Antonio Rego <antoniocostarego618@gmail.com>
2023-06-04 16:31:29 +0100

    Removed more old files

--- src/docs/Doxygen/html/search/functions_a.js
+++
@@ -1,4 +0,0 @@
-var searchData=
-[
-  ['program_0',['Program',['../class_program.html#aaefaa0df08f3484476fc4d61e97acbdc',1,'Program']]]
-];
--- src/docs/Doxygen/html/search/functions_e.js
+++
@@ -1,4 +0,0 @@
-var searchData=
-[
-  ['wait_0',['wait',['../class_program.html#a919233418f01931d025ed7b9ea47ba8c',1,'Program']]]
-];
